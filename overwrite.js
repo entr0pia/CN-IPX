ef365bfacfcbfea3de947e3104518e000bd18824
风沐白 <30486766+entr0pia@users.noreply.github.com>
2025-12-17 19:29:49 +0800

    Update overwrite.js

--- overwrite.js
+++ overwrite.js
@@ -1,4 +1,39 @@
-// Define main function (script entry)
+const newDns = {
+  "enable": true,
+  "use-system-hosts": true,
+  "use-hosts": false,
+  "ipv6": false,
+  "enhanced-mode": "redir-host",
+  "respect-rules": true,
+  "prefer-h3": false,
+  "default-nameserver": [
+    "119.29.29.29",
+    "223.5.5.5",
+    "123.125.81.6" // 最近的运营商dns
+  ],
+  "proxy-server-nameserver": [
+    "https://dns.pub/dns-query",
+    "https://dns.alidns.com/dns-query",
+    "123.125.81.6"
+  ],
+  "nameserver": [
+    "tls://1.0.0.1",
+    "tls://8.8.8.8",
+    "https://dns.google/dns-query"
+  ],
+  "nameserver-policy": {
+    "services.googleapis.cn": "tls://8.8.8.8",
+    "geosite:googlefcm": "tls://8.8.8.8",
+    "dl.google.com": "https://dns.pub/dns-query",
+    "geosite:cn": [
+      "https://dns.alidns.com/dns-query",
+      "https://dns.pub/dns-query",
+      "https://9999.alidns.com/dns-query",
+      "https://10000.alidns.com/dns-query"
+    ]
+  },
+  "fallback": null
+}
 
 const newSniffer = {
   "enable": true,
@@ -44,44 +79,7 @@ const newSniffer = {
   ]
 }
 
-const newDns = {
-  "enable": true,
-  "use-system-hosts": true,
-  "use-hosts": false,
-  "ipv6": false,
-  "enhanced-mode": "redir-host",
-  "respect-rules": true,
-  "prefer-h3": false,
-  "default-nameserver": [
-    "119.29.29.29",
-    "223.5.5.5",
-    "123.125.81.6" // 最近的运营商dns
-  ],
-  "proxy-server-nameserver": [
-    "https://dns.pub/dns-query",
-    "https://dns.alidns.com/dns-query",
-    "123.125.81.6"
-  ],
-  "nameserver": [
-    "tls://1.0.0.1",
-    "tls://8.8.8.8",
-    "https://dns.google/dns-query"
-  ],
-  "nameserver-policy": {
-    "services.googleapis.cn": "tls://8.8.8.8",
-    "geosite:googlefcm": "tls://8.8.8.8",
-    "dl.google.com": "https://dns.pub/dns-query",
-    "geosite:cn": [
-      "https://dns.alidns.com/dns-query",
-      "https://dns.pub/dns-query",
-      "https://9999.alidns.com/dns-query",
-      "https://10000.alidns.com/dns-query"
-    ]
-  },
-  "fallback": null
-}
-
-
+// Define main function (script entry)
 function main(config, profileName) {
   config['sniffer'] = { ...config['sniffer'], ...newSniffer }
   config['dns'] = { ...config['dns'], ...newDns }
